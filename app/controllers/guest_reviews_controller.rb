00b55fe5b959c0f4623e93951a8396f994b5101d
Hung Nguyen Khanh <hungnk1995@gmail.com>
2018-08-02 08:23:01 +0700

    fix_autocomplete_searching

--- app/controllers/guest_reviews_controller.rb
+++ app/controllers/guest_reviews_controller.rb
@@ -1,16 +1,16 @@
 class GuestReviewsController < ApplicationController
   before_action :check_reservation, :has_reviewed, only: [:create]
-  
+
   def create
     @guest_review = current_user.guest_reviews.create guest_review_params
-    return flash[:alert] = t("noti_error") unless @guest_review    
+    return flash[:alert] = t("noti_error") unless @guest_review
     flash[:success] = t "noti_create_review"
     redirect_back fallback_location: request.referer
   end
 
   def destroy
     @guest_review = Review.find_by id: params[:id]
-    unless @guest_review    
+    unless @guest_review
       flash[:alert] = t "noti_not_found"
       return redirect_back fallback_location: request.referer
     end
@@ -20,18 +20,19 @@ class GuestReviewsController < ApplicationController
   end
 
   private
-  
+
   def guest_review_params
-    params.require(:guest_review).permit :comment, 
+    params.require(:guest_review).permit :comment,
       :star, :room_id, :reservation_id, :host_id
   end
 
   def check_reservation
     @reservation = Reservation.check_guest_reservation(
       guest_review_params[:reservation_id],
-      guest_review_params[:room_id]).first
+      guest_review_params[:room_id]
+    ).first
     if @reservation.nil? && @reservation.room.user.id == guest_review_params[:host_id].to_i
-      flash[:alert] =  t "noti_reservation_not_found"
+      flash[:alert] = t "noti_reservation_not_found"
       return redirect_back fallback_location: request.referer
     end
   end
--- app/models/guest_review.rb
+++ app/models/guest_review.rb
@@ -1,5 +1,4 @@
 class GuestReview < Review
   belongs_to :guest, class_name: User.name
-  scope :reviewed, ->(reservation_id, host_id){
-    where reservation_id: reservation_id, host_id: host_id}
+  scope :reviewed, ->(reservation_id, host_id){where reservation_id: reservation_id, host_id: host_id}
 end
